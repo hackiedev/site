62787640d73ef9aa1e9c39cb02cb206035c5687d
Oscar Blanco <oscarbc1996@gmail.com>
2020-05-26 22:56:39 +0200

    added babelrc config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import Container from "../components/Container";
 import Text from "../components/Text";
 
 const HomePage = () => (
-  <Container fullScreen background={"#80002a"}>
-    <Text>Welcome to hackie.dev!</Text>
+  <Container fullScreen background={"#F40F49"}>
+    <Text>Welcome to sdfgsdf.dev!</Text>
   </Container>
 );
 
